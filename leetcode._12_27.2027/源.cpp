420612347171a270853335d29e4b2c5637d9bdc2
lijinggai <2786810048@qq.com>
2022-12-27 18:41:27 +0800

    leetcode._12_27.2027/

---
+++ leetcode._12_27.2027/源.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int ans = 0, n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'X')
+            {
+                ans++;
+                i += 2;
+            }
+        }
+        return ans;
+    }
+};
