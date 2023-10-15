89447c2fd4f0cfc240d4fc744cfa420242284217
lijinggai <2786810048@qq.com>
2023-10-15 21:05:06 +0800

    leetcode_553/

---
+++ leetcode_553/源.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return to_string(nums[0]);
+        if (n == 2)
+            return to_string(nums[0]) + "/" + to_string(nums[1]);
+        string res = to_string(nums[0]) + "/(";
+        for (int i = 1; i < n; i++)
+        {
+            res += to_string(nums[i]);
+            res += "/";
+        }
+        res[res.size() - 1] = ')';
+        return res;
+    }
+};
